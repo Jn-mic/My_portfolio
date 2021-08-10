71bfb4fff2a3adc07c32f52708f2976520209faa
jn-mic <jackotienokey@gmail.com>
2021-08-10 14:40:46 +0300

    adding features on my portfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import sys
+sys.path.append("..")
+# from users.models import User
+from django.http import HttpResponse
+from django.views import generic 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+# from .email import send_welcome_email
+import datetime as dt
 
 # Create your views here.
+def Home(request):
+
+    return render(request,'portfolio/home.html')
+
+# def HomePageView(request):
+#     return render(request,'portfolio/home.html')
